debef024de89cb160151ff93a709230869830347
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-10-12 13:41:07 +0200

    add data.py

---
+++ modules/data.py
@@ -0,0 +1,5 @@
+# global variables
+french_metropolitan_department_numbers = list(range(1, 96))
+print(french_metropolitan_department_numbers)
+french_overseas_department_numbers = [971, 972, 973, 974, 975, 976, 977, 978, 984, 986, 987, 988, 989]
+print(french_overseas_department_numbers)
