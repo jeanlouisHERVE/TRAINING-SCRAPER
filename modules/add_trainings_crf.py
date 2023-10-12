dfff5be59e5c4124b7e53f0619960b75568c45c6
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-10-12 18:03:46 +0200

    start implementing add trainings part

--- modules/add_trainings_crf.py
+++ modules/add_trainings_crf.py
@@ -1,7 +1,7 @@
 # packages
 # import os
 # import re
-import time
+# import time
 import pytz
 import datetime
 from selenium.webdriver.common.by import By
@@ -51,7 +51,7 @@ def check_accept_section(cssSelector: str):
         print("KO : no accept part")
 
 
-def add_new_announces():
+def add_trainings():
     print("------------------Add_crf_training_Start------------------")
     # connection to website
     # TODO add url
@@ -59,10 +59,15 @@ def add_new_announces():
         driver.get(f"https://inscription-formation.croix-rouge.fr/?type=PSC1&dep={department_number}")
         driver.implicitly_wait(5)
         # check an agree the terms section exists
-        time.sleep(2)
-        # TODO if concerned add css class
-        check_accept_section()
-        time.sleep(2)
+        try:
+            trainingsList = driver.find_element(By.CSS_SELECTOR, "div.liste-formations")
+            trainings = trainingsList.find_elements(By.XPATH, ".//h2/following-sibling::ul")
+            pairs = []
+            for i in range(0, len(trainings), 2):
+                pairs.append((trainings[i], trainings[i + 1]))
+            print(pairs)
+        except NoSuchElementException:
+            print("KO : no list of trainings on this page")
 
     print("------------------Add_crf_training_End------------------")
 
--- modules/data.py
+++ modules/data.py
@@ -1,7 +1,4 @@
 # global variables
 french_metropolitan_department_numbers = [str(num).zfill(2) for num in list(range(1, 96))]
-print(french_metropolitan_department_numbers)
 french_overseas_department_numbers = [971, 972, 973, 974, 975, 976, 977, 978, 984, 986, 987, 988, 989]
-print(french_overseas_department_numbers)
 french_department_numbers = french_metropolitan_department_numbers + french_overseas_department_numbers
-print(french_department_numbers)
