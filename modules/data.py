204d24e3026844aea7bd2a67b40845fd9f9a477f
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-10-12 13:49:59 +0200

    update french metropolitan department numbers variable

--- modules/data.py
+++ modules/data.py
@@ -1,5 +1,5 @@
 # global variables
-french_metropolitan_department_numbers = list(range(1, 96))
+french_metropolitan_department_numbers = [str(num).zfill(2) for num in list(range(1, 96))]
 print(french_metropolitan_department_numbers)
 french_overseas_department_numbers = [971, 972, 973, 974, 975, 976, 977, 978, 984, 986, 987, 988, 989]
 print(french_overseas_department_numbers)
