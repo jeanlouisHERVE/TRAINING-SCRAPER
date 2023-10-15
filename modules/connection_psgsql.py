76879d31bceffd262397ed1a16b0af83ff5157dd
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-10-15 10:30:15 +0200

    work on database connection to server

--- modules/connection_psgsql.py
+++ modules/connection_psgsql.py
@@ -5,12 +5,21 @@ from dotenv import load_dotenv
 
 DATABASE_PROMPT = "Enter the DATABASE_URI value or leave empty to load from .env file: "
 
+db_params = {
+    'dbname': 'trainingscraper',
+    'user': 'postgres',
+    'password': os.environ["DATABASE_PASSWORD"],
+    'host': 'localhost',
+    'port': '5432'
+}
+
 database_uri = input(DATABASE_PROMPT)
 if not database_uri:
     load_dotenv()
     database_uri = os.environ["DATABASE_URI"]
 
-pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=database_uri)
+# pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=database_uri)
+pool = SimpleConnectionPool(minconn=1, maxconn=10, **db_params)
 
 
 # context manager
