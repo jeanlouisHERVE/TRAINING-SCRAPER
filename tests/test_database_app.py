dbe09dc0fe9649d60c1056bf105ff3adaa68cdec
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-10-18 20:33:54 +0200

    add test for add data to database

--- tests/test_database_app.py
+++ tests/test_database_app.py
@@ -18,7 +18,7 @@ from modules.database_app import (
     get_towns,
     get_organisms
 )
-# TODO add functions to test
+
 # TODO adapt to postgresql
 
 # get data from .env file
@@ -62,116 +62,110 @@ class TestDatabaseFunctions(unittest.TestCase):
             print("-------------------------------------------------------")
             print("-------------------------------------------------------")
 
+    def test_add_course(self):
+        places_available = 10
+        places_total = 20
+        price = 100
+        date_add_to_db = 1634520000
+        town_id = 1
+        type_id = 2
+        organism_id = 3
+        department_id = 4
+        date_id = 5
 
-def test_add_course(self):
-    places_available = 10
-    places_total = 20
-    price = 100
-    date_add_to_db = 1634520000
-    town_id = 1
-    type_id = 2
-    organism_id = 3
-    department_id = 4
-    date_id = 5
+        last_inserted_id = add_course(
+            places_available,
+            places_total,
+            price,
+            date_add_to_db,
+            town_id,
+            type_id,
+            organism_id,
+            department_id,
+            date_id,
+        )
 
-    last_inserted_id = add_course(
-        places_available,
-        places_total,
-        price,
-        date_add_to_db,
-        town_id,
-        type_id,
-        organism_id,
-        department_id,
-        date_id,
-    )
+        self.assertIsNotNone(last_inserted_id)
+        self.cur.execute("SELECT * FROM courses WHERE id = %s", (last_inserted_id,))
+        result = self.cur.fetchone()
+        self.assertIsNotNone(result)
+        self.assertEqual(result[1], places_available)
+        self.assertEqual(result[2], places_total)
+        self.assertEqual(result[3], price)
+        self.assertEqual(result[4], date_add_to_db)
+        self.assertEqual(result[5], town_id)
+        self.assertEqual(result[6], type_id)
+        self.assertEqual(result[7], organism_id)
+        self.assertEqual(result[8], department_id)
+        self.assertEqual(result[9], date_id)
 
-    self.assertIsNotNone(last_inserted_id)
-    self.cur.execute("SELECT * FROM courses WHERE id = %s", (last_inserted_id,))
-    result = self.cur.fetchone()
-    self.assertIsNotNone(result)
-    self.assertEqual(result[1], places_available)
-    self.assertEqual(result[2], places_total)
-    self.assertEqual(result[3], price)
-    self.assertEqual(result[4], date_add_to_db)
-    self.assertEqual(result[5], town_id)
-    self.assertEqual(result[6], type_id)
-    self.assertEqual(result[7], organism_id)
-    self.assertEqual(result[8], department_id)
-    self.assertEqual(result[9], date_id)
+    def test_add_date(self):
+        hour_start = "08:00 AM"
+        hour_end = "04:00 PM"
+        date = 1634606400
 
+        last_inserted_id = add_date(hour_start, hour_end, date)
 
-def test_add_date(self):
-    hour_start = "08:00 AM"
-    hour_end = "04:00 PM"
-    date = 1634606400
+        self.assertIsNotNone(last_inserted_id)
 
-    last_inserted_id = add_date(hour_start, hour_end, date)
+        self.cur.execute("SELECT * FROM dates WHERE id = %s", (last_inserted_id,))
+        result = self.cur.fetchone()
+        self.assertIsNotNone(result)
+        self.assertEqual(result[1], hour_start)
+        self.assertEqual(result[2], hour_end)
+        self.assertEqual(result[3], date)
 
-    self.assertIsNotNone(last_inserted_id)
+    def test_add_training(self):
+        name = "Test Training"
+        description = "A training description"
 
-    self.cur.execute("SELECT * FROM dates WHERE id = %s", (last_inserted_id,))
-    result = self.cur.fetchone()
-    self.assertIsNotNone(result)
-    self.assertEqual(result[1], hour_start)
-    self.assertEqual(result[2], hour_end)
-    self.assertEqual(result[3], date)
+        last_inserted_id = add_training(name, description)
 
+        self.assertIsNotNone(last_inserted_id)
 
-def test_add_training(self):
-    name = "Test Training"
-    description = "A training description"
+        self.cur.execute("SELECT * FROM training WHERE id = %s", (last_inserted_id,))
+        result = self.cur.fetchone()
+        self.assertIsNotNone(result)
+        self.assertEqual(result[1], name)
+        self.assertEqual(result[2], description)
 
-    last_inserted_id = add_training(name, description)
+    def test_add_department(self):
+        number = "123"
+        name = "Test Department"
 
-    self.assertIsNotNone(last_inserted_id)
+        last_inserted_id = add_department(number, name)
 
-    self.cur.execute("SELECT * FROM training WHERE id = %s", (last_inserted_id,))
-    result = self.cur.fetchone()
-    self.assertIsNotNone(result)
-    self.assertEqual(result[1], name)
-    self.assertEqual(result[2], description)
+        self.assertIsNotNone(last_inserted_id)
 
+        self.cur.execute("SELECT * FROM department WHERE id = %s", (last_inserted_id,))
+        result = self.cur.fetchone()
+        self.assertIsNotNone(result)
+        self.assertEqual(result[1], number)
+        self.assertEqual(result[2], name)
 
-def test_add_department(self):
-    number = "123"
-    name = "Test Department"
+    def test_add_town(self):
+        name = "Test Town"
 
-    last_inserted_id = add_department(number, name)
+        last_inserted_id = add_town(name)
 
-    self.assertIsNotNone(last_inserted_id)
+        self.assertIsNotNone(last_inserted_id)
 
-    self.cur.execute("SELECT * FROM department WHERE id = %s", (last_inserted_id,))
-    result = self.cur.fetchone()
-    self.assertIsNotNone(result)
-    self.assertEqual(result[1], number)
-    self.assertEqual(result[2], name)
+        self.cur.execute("SELECT * FROM town WHERE id = %s", (last_inserted_id,))
+        result = self.cur.fetchone()
+        self.assertIsNotNone(result)
+        self.assertEqual(result[1], name)
 
+    def test_add_organism(self):
+        name = "Test Organism"
 
-def test_add_town(self):
-    name = "Test Town"
+        last_inserted_id = add_organism(name)
 
-    last_inserted_id = add_town(name)
+        self.assertIsNotNone(last_inserted_id)
 
-    self.assertIsNotNone(last_inserted_id)
-
-    self.cur.execute("SELECT * FROM town WHERE id = %s", (last_inserted_id,))
-    result = self.cur.fetchone()
-    self.assertIsNotNone(result)
-    self.assertEqual(result[1], name)
-
-
-def test_add_organism(self):
-    name = "Test Organism"
-
-    last_inserted_id = add_organism(name)
-
-    self.assertIsNotNone(last_inserted_id)
-
-    self.cur.execute("SELECT * FROM organism WHERE id = %s", (last_inserted_id,))
-    result = self.cur.fetchone()
-    self.assertIsNotNone(result)
-    self.assertEqual(result[1], name)
+        self.cur.execute("SELECT * FROM organism WHERE id = %s", (last_inserted_id,))
+        result = self.cur.fetchone()
+        self.assertIsNotNone(result)
+        self.assertEqual(result[1], name)
 
 
 if __name__ == '__main__':
